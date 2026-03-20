ec3752aaac315e18f99c047ebef5241c75839cf9
Daniel Taillefer <danieltaillefernieto@gmail.com>
2026-03-20 16:32:34 +0100

    Criterio 2: Rutas y componentes.  Realizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { UsuarioFormComponent } from './pages/usuario-form/usuario-form.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', pathMatch:'full', redirectTo:'home'},
+    {path:'home', component: HomeComponent},
+    {path: 'user/:id' , component: UsuarioComponent },
+    {path: 'newuser', component: UsuarioFormComponent},
+    {path: 'updateuser/:id', component: UsuarioFormComponent},
+    { path: "**", component: Error404Component }
+];
